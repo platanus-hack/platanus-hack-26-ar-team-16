b3a95c5c24ea99d442645c031b02b9f601405f95
Juan Pablo Mantegazza <juampi.mantegazza@gmail.com>
2026-05-09 18:52:21 -0300

    fix: use top-level babel-preset-expo instead of nested expo path

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      [require('expo/node_modules/babel-preset-expo'), { jsxImportSource: 'nativewind' }],
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
   };
